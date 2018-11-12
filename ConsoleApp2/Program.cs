b7ce6d9a69f9ddc91efb3554bdc644d61568c122
deathka1990 <deathka1990@gmail.com>
2018-11-12 21:15:46 +0300

    some changes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,101 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number of lines in the array: ");
+            int lines = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number of columns in the array: ");
+            int columns = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter min number for random generation:");
+            int min = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter max number for random generation:");
+            int max = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+
+
+            int[,] bubaArray = new int[lines, columns];
+            var rnd = new Random();
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    bubaArray[i, j] = rnd.Next(min, max + 1);
+                    Console.Write("{0}\t", bubaArray[i, j]);
+                }
+                Console.WriteLine("\n");
+            }
+
+
+            Console.WriteLine("\n Bubble Sort \n ");
+
+            BubbleSort(bubaArray, lines, columns);
+
+
+
+
+
+
+               
+
+            Console.ReadLine();
         }
+
+
+
+        static void BubbleSort(int[,] bubaArray, int lines, int columns)
+        {
+            int[] tempArray = new int[lines * columns];
+            int kostyl = 0;
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    tempArray[kostyl] = bubaArray[i, j];
+
+
+                    kostyl++;
+                }
+            }
+
+            Console.Write("\n");
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                for (int j = 0; j < tempArray.Length - 1; j++)
+                {
+                    if (tempArray[j] > tempArray[j + 1])
+                    {
+                        int znach = tempArray[j];
+                        tempArray[j] = tempArray[j + 1];
+                        tempArray[j + 1] = znach;
+
+                    }
+                }
+            }
+            Console.Write("\n");
+            int l = 0;
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+
+                    bubaArray[i, j] = tempArray[l];
+                    l++;
+                    Console.Write("{0}\t", bubaArray[i, j]);
+                }
+                Console.WriteLine("\n");
+            }
+
+
+
+
+
+        }
+
+
+
+
+
     }
 }
